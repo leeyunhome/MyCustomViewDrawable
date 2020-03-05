7838a390a4ea91f47b40318ce6b7edda8994088b
leeyunhome <leeyunhome@daum.net>
2020-03-05 10:03:50 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/customviewdrawable/MainActivity.java
@@ -0,0 +1,16 @@
+package com.egloos.customviewdrawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        CustomViewDrawable view = new CustomViewDrawable(this);
+        setContentView(view);
+    }
+}
